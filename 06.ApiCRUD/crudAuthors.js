994845089528ef05887854bc0a41d619a488dbc6
kitoyjami <weurbanor@unitru.edu.pe>
2022-03-29 01:09:55 -0500

    Se uso cruc

---
+++ 06.ApiCRUD/crudAuthors.js
@@ -0,0 +1,40 @@
+//! GOOD READS API 
+//* DOCUMENTACIÓN: https://goodreads-devf-aaron.herokuapp.com/docs/
+//* URI DE LA API: https://goodreads-devf-aaron.herokuapp.com/api/v1/
+
+// TRAER A LOS PAQUETES QUE ACABAMOS DE INSTALAR REQUEST & COLORS
+const request = require ('request')
+var color = require ('colors')
+
+//TENER NUESTRO ENDPOINT 
+const URI = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/'
+
+
+// LISTAR A TODOS LOS AUTORES (AUTHORS)
+const listAuthors = () => {
+  request.get(URI, (error, response, body) => {
+    if(response.statusCode === 200){
+        const authors = JSON.parse(body)
+        console.log(authors)
+    }else{
+        console.log(response.statusCode , response.statusMessage)
+        //EJEMPLO: 404 NOT FOUND
+    }
+  })
+}
+
+// LISTAR UN AUTOR POR ID 
+const getAuthor = (id) => {
+    request.get(URI+id+'/', (error,response,body)=>{
+        if(response.statusCode === 200){
+            const author = JSON.parse(body)
+            console.log(author)
+        }else{
+            console.log("ID NO VALIDO ", response.statusCode, response.statusMessage.red)
+            //EJEMPLO: 404 NOT FOUND
+        }
+    })
+}
+module.exports={
+    listAuthors,getAuthor
+}
---
+++ 06.ApiCRUD/index.js
@@ -0,0 +1,10 @@
+// index principal 
+const gooReadsCrud =require('./crudAuthors.js');
+
+//Lista de autores 
+
+//gooReadsCrud.listAuthors();
+
+// Ver un author 
+
+gooReadsCrud.getAuthor(14567);
