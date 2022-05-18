df0faeea658ef590d32c2bcf9b5fac05b0f4687e
kitoyjami <weurbanor@unitru.edu.pe>
2022-05-18 00:11:43 -0500

    mod: arreglo endpoint

--- buscar-series/src/services/getMovies.js
+++ buscar-series/src/services/getMovies.js
@@ -5,5 +5,6 @@ const { VITE_API_KEY } = import.meta.env
 
 export const getMovies = async ({ query }) => {
   const req = await axios.get(`http://www.omdbapi.com/?apikey=${VITE_API_KEY}&s=${query}`)
-  return req.data
+  const res = req.data.Response === 'True' ? req.data.Search : []
+  return res
 }
